3304cc08f773a84ee5f7a2a16305f95cb2991d66
Rico <d-Rickyy-b@users.noreply.github.com>
2019-05-14 21:31:49 +0200

    TASK: Rename method

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def download_issues(filename="domains.json", skip=0, only_active=False, only_wit
         tp.kill()
 
     # Wait until all the threads are finished
-    tp.join_threads()
+    tp._join_threads()
 
     while not domain_queue.empty():
         domains.append(domain_queue.get())
